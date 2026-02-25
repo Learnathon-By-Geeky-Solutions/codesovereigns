f4deb97dc8ba6d71d8f214859acef9ed40afd7b0
INJAMAM <injamamul937@gmail.com>
2026-02-25 17:21:39 +0600

    change title

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,18 +1,20 @@
 function Footer() {
-    return (
-        <div>
-            <hr />
-            <div className="p-5 flex flex-col gap-3 justify-center items-center">
-                {/* <div>Contact: 01795019802</div> */}
-                {/* <div>Email: riadkabir45@gmail.com</div> */}
-                <div className="flex flex-col sm:flex-row gap-2 text-center">
-                    <div>Copyright © 2024 ByteBit</div><span className="hidden sm:inline">|</span>
-                    <div>All Rights Reserved</div><span className="hidden sm:inline">|</span>
-                    <div>Bangladesh</div>
-                </div>
-            </div>
+  return (
+    <div>
+      <hr />
+      <div className="p-5 flex flex-col gap-3 justify-center items-center">
+        {/* <div>Contact: 01795019802</div> */}
+        {/* <div>Email: riadkabir45@gmail.com</div> */}
+        <div className="flex flex-col sm:flex-row gap-2 text-center">
+          <div>Copyright © 2024 ElectroBay</div>
+          <span className="hidden sm:inline">|</span>
+          <div>All Rights Reserved</div>
+          <span className="hidden sm:inline">|</span>
+          <div>Bangladesh</div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
